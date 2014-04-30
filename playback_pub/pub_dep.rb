2099bf99834d449900a378551383185a89a19212
Nicolas Viennot <nicolas@viennot.biz>
2014-04-30 01:33:07 -0400

    updates

--- playback_pub/pub_dep.rb
+++ playback_pub/pub_dep.rb
@@ -20,14 +20,14 @@ $msg_count_bench = Stats::Counter.new('pub_msg')
 
 def create_post(user_id)
   current_user = User.new(:id => user_id)
-  # Promiscuous::Publisher::Context.current.current_user = current_user
+  Promiscuous::Publisher::Context.current.current_user = current_user
 
   post = Post.new(:author_id => user_id, :content => 'hello world')
   if post.is_a?(Promiscuous::Publisher::Model::Ephemeral)
     post.id = "#{user_id}0#{current_user.node.incr("pub:#{user_id}:latest_post_id")}"
   end
 
-  $overhead_stat.measure { post.save }
+  $overhead_stat.measure { user.save }
   $msg_count_bench.inc
 
   unless post.is_a?(Promiscuous::Publisher::Model::Ephemeral)
