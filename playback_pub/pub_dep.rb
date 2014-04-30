fa27fb02c51a7a78d15a08d21b1cb2c3cf8b0b3b
Nicolas Viennot <nicolas@viennot.biz>
2014-04-30 01:31:44 -0400

    updates

--- playback_pub/pub_dep.rb
+++ playback_pub/pub_dep.rb
@@ -18,22 +18,27 @@ $num_users = 2**30 if $num_users == 0
 $overhead_stat = Stats::Average.new('pub_overhead')
 $msg_count_bench = Stats::Counter.new('pub_msg')
 
+def create_post(user_id)
+  current_user = User.new(:id => user_id)
+  Promiscuous::Publisher::Context.current.current_user = current_user
+
+  post = Post.new(:author_id => user_id, :content => 'hello world')
+  if post.is_a?(Promiscuous::Publisher::Model::Ephemeral)
+    post.id = "#{user_id}0#{current_user.node.incr("pub:#{user_id}:latest_post_id")}"
+  end
+
+  $overhead_stat.measure { post.save }
+  $msg_count_bench.inc
+
+  unless post.is_a?(Promiscuous::Publisher::Model::Ephemeral)
+    current_user.node.set("pub:#{user_id}:latest_post_id", post.id)
+  end
+end
+
 def publish
   loop do
     Promiscuous.context(:bench) do
-      user_id = rand(1..$num_users)
-      current_user = User.new(:id => user_id)
-      Promiscuous::Publisher::Context.current.current_user = current_user
-
-      $num_read_deps.times { User.new(:id => rand(1..$num_users)).read }
-
-      post = Post.new(:author_id => user_id, :content => 'hello world')
-      if post.is_a?(Promiscuous::Publisher::Model::Ephemeral)
-        post.id = "#{user_id}0#{current_user.node.incr("pub:#{user_id}:latest_post_id")}"
-      end
-
-      $overhead_stat.measure { post.save }
-      $msg_count_bench.inc
+      create_post(1)
     end
   end
 end
