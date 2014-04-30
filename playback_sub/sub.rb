f9b77bf4f47f85e354e283133f78983460e8ffbf
Nicolas Viennot <nicolas@viennot.biz>
2014-04-30 01:45:11 -0400

    updates

--- playback_sub/sub.rb
+++ playback_sub/sub.rb
@@ -3,9 +3,16 @@ load 'common.rb'
 
 bootstrap(:sub)
 
+$msg_count_bench = Stats::Counter.new('sub_msg')
 class User
   include Promiscuous::Subscriber
+  after_save do
+$msg_count_bench.inc
+sleep ENV['SUB_LATENCY'].to_f if ENV['SUB_LATENCY']
+
+  end
 end
+
 
 finalize_bootstrap(:sub)
 sleep 100000
